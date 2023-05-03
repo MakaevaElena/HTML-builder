bc61e59706685b58716a2cee88eba23e80633e82
yamaslena@yandex.ru <yamaslena@yandex.ru>
2023-05-03 17:28:35 +0700

    feat: 04-copy-directory solved

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,24 @@
+const fs = require('fs');
+const path = require('path');
 
+const copyDir = () => {
+  fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
+    if (err) throw err; // не удалось создать папку
+    console.log(`Папка files-copy успешно создана`);
+  });
+
+  fs.readdir(path.join(__dirname, 'files'), (_, files) => {
+    files.forEach((file) => {
+      fs.copyFile(
+        path.join(__dirname, 'files', file),
+        path.join(__dirname, 'files-copy', file),
+        (err) => {
+          if (err) throw err; // не удалось скопировать файл
+          console.log(`Файл ${file} успешно скопирован`);
+        },
+      );
+    });
+  });
+};
+
+copyDir();
