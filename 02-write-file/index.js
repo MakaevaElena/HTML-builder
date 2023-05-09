79bfe8c83026284ece30f0b1e0282ee3702a25da
yamaslena@yandex.ru <yamaslena@yandex.ru>
2023-05-09 15:44:33 +0700

    fix: 02-write-file message

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -6,18 +6,23 @@ const output = fs.createWriteStream(path.join(__dirname, 'text.txt'));
 const { stdin, stdout } = process;
 stdout.write('Привет! Напиши что-нибудь\n');
 
+process.on('SIGINT', () => {
+  // stdout.write('Ваш текст записан в файл text.txt\n');
+  process.exit();
+});
+
 stdin.on('data', (data) => {
   if (data.toString().match('exit')) {
-    stdout.write('Buy -buy!');
+    // stdout.write('Buy-buy!');
     process.exit();
   }
 
   output.write(data);
   process.on('SIGINT', () => {
     // stdout.write('\n');
-    stdout.write('Ваш текст записан в файл text.txt\n');
+    // stdout.write('Ваш текст записан в файл text.txt\n');
     process.exit();
   });
 });
 
-// process.on('exit', () => stdout.write('Buy -buy!'));
+process.on('exit', () => stdout.write('Buy-buy!'));
